ab588f15c9b9a238af623e92cbda934f6ecefbe4
KdAndrade <kauand.andrade17@gmail.com>
2025-06-20 19:07:31 -0300

    Update DadosCliente.java

--- PROJETOA3/src/entity/DadosCliente.java
+++ PROJETOA3/src/entity/DadosCliente.java
@@ -1,53 +1,112 @@
 package entity;
 
+import java.text.ParseException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import javax.swing.text.MaskFormatter;
 
 public class DadosCliente {
+
     Scanner leia = new Scanner(System.in);
-    
-    private String nome ;
-    private String telefone;;
+
+    private String nome;
+    private String telefone;
     private String email;
-     private String cpf;
+    private String cpf;
     private String Endereco;
+
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+
+    private String formatarString(String valor, String mascara) {
+        try {
+            MaskFormatter mf = new MaskFormatter(mascara);
+            mf.setValueContainsLiteralCharacters(false);
+            return mf.valueToString(valor);
+        } catch (ParseException ex) {
+            return valor;
+        }
+    }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail() {
-        System.out.println("Digite o seu email:");
-        email=leia.nextLine();
+        while (true) {
+            System.out.println("Digite o seu email:");
+            String emailInput = leia.nextLine();
+            if (EMAIL_PATTERN.matcher(emailInput).matches()) {
+                this.email = emailInput;
+                break;
+            } else {
+                System.out.println("\nERRO");
+                System.out.println("Formato de e-mail inválido.");
+                System.out.println("O formato correto é: nome@dominio.com");
+                System.out.println("Por favor, digite novamente.\n");
+            }
+        }
     }
-   
+
     public String getEndereco() {
         return Endereco;
     }
+
     public void setEndereco() {
         System.out.println("Informe o endereço: ");
         Endereco = leia.nextLine();
     }
-   
+
     public String getNome() {
         return nome;
     }
+
     public void setNome() {
         System.out.println("Digite o nome a ser cadastrado: ");
         nome = leia.nextLine();
     }
+
     public String getTelefone() {
-        
         return telefone;
     }
+
     public void setTelefone() {
-        System.out.println("Digite o telefone a ser cadastrado: ");
-        telefone = leia.nextLine();
-       
+        while (true) {
+            System.out.println("Digite o telefone (apenas números):");
+            String telefoneInput = leia.nextLine();
+            String telefoneNumerico = telefoneInput.replaceAll("[^0-9]", "");
+
+            if (telefoneNumerico.length() == 11) {
+                this.telefone = formatarString(telefoneNumerico, "(12) 12345-1234");
+                break;
+            } else {
+                System.out.println("\n--- ERRO ---");
+                System.out.println("Formato de telefone inválido.");
+                System.out.println("O formato correto deve conter 11 números: (##) #####-####");
+                System.out.println("Por favor, digite novamente.\n");
+            }
+        }
     }
+
     public String getCpf() {
         return cpf;
     }
+
     public void setCpf() {
-        System.out.println("Digite o CPF: ");
-        cpf = leia.nextLine();
+        while (true) {
+            System.out.println("Digite o CPF (apenas números):");
+            String cpfInput = leia.nextLine();
+            String cpfNumerico = cpfInput.replaceAll("[^0-9]", "");
+
+            if (cpfNumerico.length() == 11) {
+                this.cpf = formatarString(cpfNumerico, "###.###.###-##");
+                break;
+            } else {
+                System.out.println("\nERRO");
+                System.out.println("Formato de CPF inválido.");
+                System.out.println("O formato correto deve conter 11 números: ###.###.###-##");
+                System.out.println("Por favor, digite novamente.\n");
+            }
+        }
     }
-    
 }

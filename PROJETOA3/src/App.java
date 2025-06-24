f275336e2e13fff8d63e3823885058a5fd25e6d4
Marcos Gabriel Alves dos Santos <marcos.gabriel.doth15@gmail.com>
2025-06-24 20:36:42 -0300

    Atualizando a quantidade de vezes que pode inserir

--- PROJETOA3/src/App.java
+++ PROJETOA3/src/App.java
@@ -5,19 +5,32 @@ import produto.DescricaoProduto;
 public class App {
     public static void main(String[] args) throws Exception {
        DadosCliente u = new DadosCliente();
-       DescricaoProduto c = new DescricaoProduto();
-       c.setModelocamisa();
+       DescricaoProduto c = new DescricaoProduto(); //TEM QUE COLOCAR UMA CONDIÇÃO PARA CASO QUEIRA FAZER MAIS DE UM PEDIDO EM UM SÓ "RUN"
+       int i=0;
+       c.setQuantidade();
+       
+       while(i <c.getQuantidade() ){
+         c.setModelocamisa();
        c.setTecido();
        c.setTamanho();
        c.setValorunitario();
-       c.setQuantidade();
+       
        c.setValortotal();
+       c.setFormadepagamento();
+       c.setFormadeentrega();
+       c.setPrazodeentrega();
+       c.setStatus();
+
+
        
        u.setNome();
        u.setTelefone();
        u.setEmail();
        u.setCpf();
        u.setEndereco();
+       i++;
+       }
+      
        
       
        

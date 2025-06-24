72570b614bb4b7d7224c20f4ab479eae82819ca7
Marcos Gabriel Alves dos Santos <marcos.gabriel.doth15@gmail.com>
2025-06-24 20:37:59 -0300

    Update UsuarioDAO.java

--- PROJETOA3/src/DAO/UsuarioDAO.java
+++ PROJETOA3/src/DAO/UsuarioDAO.java
@@ -4,49 +4,55 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+import cadastrar.DescricaoDAO;
 import conexao.Conexao;
 import entity.DadosCliente;
+
 public class UsuarioDAO {
-    public int cadastrarUsuario(DadosCliente dadoscliente){
-   String sql = "INSERT INTO DADOSCLIENTE (NOME,TELEFONE,EMAIL, CPF, ENDEREÇO) VALUES (?,?,?,?,?)";
-   String sql2 = "INSERT INTO PEDIDO_CLIENTE_PRODUTO (idpedido_produto,idpedido_cliente,formadepagamento) VALUES (?,?,?)";
-   PreparedStatement es = null;
-   try(PreparedStatement ps = Conexao.getConexao().prepareStatement(sql,Statement.RETURN_GENERATED_KEYS)) {
-   
-    ps.setString(1, dadoscliente.getNome());
-    ps.setString(2, dadoscliente.getTelefone());
-    ps.setString(3, dadoscliente.getEmail());
-    ps.setString(4, dadoscliente.getCpf());
-     ps.setString(5, dadoscliente.getEndereco());
-    ps.executeUpdate();
-    ResultSet rs = ps.getGeneratedKeys();
-    if(rs.next()){
-        int idCliente = rs.getInt(1);
-       System.out.println("A inserção de dados foi concluida com sucesso! \n id ="+idCliente);  
-       return idCliente;
+    public int cadastrarUsuario(DadosCliente dadoscliente) {
         
+        int idproduto = DescricaoDAO.idProduto;
+        int idcliente=-1;
+        String sql = "INSERT INTO DADOSCLIENTE (NOME,TELEFONE,EMAIL, CPF, ENDEREÇO) VALUES (?,?,?,?,?)";
+        String sql2 = "INSERT INTO PEDIDO_CLIENTE_PRODUTO (idpedido_produto,idpedido_cliente) VALUES (?,?)";
+       
+        try (PreparedStatement ps = Conexao.getConexao().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+            ps.setString(1, dadoscliente.getNome());
+            ps.setString(2, dadoscliente.getTelefone());
+            ps.setString(3, dadoscliente.getEmail());
+            ps.setString(4, dadoscliente.getCpf());
+            ps.setString(5, dadoscliente.getEndereco());
+            ps.executeUpdate();
+            
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                int idCliente = rs.getInt(1);
+                System.out.println("A inserção de dados foi concluida com sucesso! \n id =" + idCliente);
+                idcliente = idCliente;
+                try (PreparedStatement es = Conexao.getConexao().prepareStatement(sql2)) {
+                es.setInt(1, idproduto);
+                es.setInt(2, idcliente);
+                es.executeUpdate();
+            }
+            return idcliente;
+                
+
+            }/*es = Conexao.getConexao().prepareStatement(sql2);
+            idproduto = DescricaoDAO.idProduto;
+            es.setInt(1, idproduto);
+            es.setInt(2, idcliente);
+            es.executeUpdate();
+            es.close(); */
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erro no usuario");
+        }
+       
+
+        return -1;
+
     }
-   
-} catch (SQLException e) {
-    e.printStackTrace();
-    System.out.println("Erro no usuario");
-}
-
-
-try {
-     es = Conexao.getConexao().prepareStatement(sql2);
-     es.executeUpdate();
-} catch (Exception e) {
-     e.printStackTrace();
-}
-return -1;
-
-    }
-
-
 
 }
-
-
-

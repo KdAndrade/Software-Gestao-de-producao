fd9a3186c774df37be0ceaf694e7d36ed9daecec
Marcos Gabriel Alves dos Santos <marcos.gabriel.doth15@gmail.com>
2025-06-24 21:25:18 -0300

    Update UsuarioDAO.java

--- PROJETOA3/src/DAO/UsuarioDAO.java
+++ PROJETOA3/src/DAO/UsuarioDAO.java
@@ -10,49 +10,37 @@ import entity.DadosCliente;
 
 public class UsuarioDAO {
     public int cadastrarUsuario(DadosCliente dadoscliente) {
-        
-        int idproduto = DescricaoDAO.idProduto;
-        int idcliente=-1;
+        int idcliente = -1;
         String sql = "INSERT INTO DADOSCLIENTE (NOME,TELEFONE,EMAIL, CPF, ENDEREÇO) VALUES (?,?,?,?,?)";
-        String sql2 = "INSERT INTO PEDIDO_CLIENTE_PRODUTO (idpedido_produto,idpedido_cliente) VALUES (?,?)";
-       
-        try (PreparedStatement ps = Conexao.getConexao().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
 
+        try (PreparedStatement ps = Conexao.getConexao().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             ps.setString(1, dadoscliente.getNome());
             ps.setString(2, dadoscliente.getTelefone());
             ps.setString(3, dadoscliente.getEmail());
             ps.setString(4, dadoscliente.getCpf());
             ps.setString(5, dadoscliente.getEndereco());
             ps.executeUpdate();
-            
+
             ResultSet rs = ps.getGeneratedKeys();
             if (rs.next()) {
-                int idCliente = rs.getInt(1);
-                System.out.println("A inserção de dados foi concluida com sucesso! \n id =" + idCliente);
-                idcliente = idCliente;
-                try (PreparedStatement es = Conexao.getConexao().prepareStatement(sql2)) {
-                es.setInt(1, idproduto);
-                es.setInt(2, idcliente);
-                es.executeUpdate();
+                idcliente = rs.getInt(1);
             }
-            return idcliente;
-                
-
-            }/*es = Conexao.getConexao().prepareStatement(sql2);
-            idproduto = DescricaoDAO.idProduto;
-            es.setInt(1, idproduto);
-            es.setInt(2, idcliente);
-            es.executeUpdate();
-            es.close(); */
-
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Erro no usuario");
         }
-       
 
-        return -1;
-
+        return idcliente;
     }
 
+    public void cadastrarPedidoClienteProduto(int idProduto, int idCliente) {
+        String sql = "INSERT INTO PEDIDO_CLIENTE_PRODUTO (idpedido_produto, idpedido_cliente) VALUES (?,?)";
+        try (PreparedStatement ps = Conexao.getConexao().prepareStatement(sql)) {
+            ps.setInt(1, idProduto);
+            ps.setInt(2, idCliente);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }

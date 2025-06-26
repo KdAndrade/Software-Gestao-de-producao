53266d41f10ad8774cb213df80063bbacdb8c173
Marcos Gabriel Alves dos Santos <marcos.gabriel.doth15@gmail.com>
2025-06-26 18:34:58 -0300

    Create Contador.java

---
+++ PROJETOA3/src/testemateriaprima/Contador.java
@@ -0,0 +1,44 @@
+package testemateriaprima;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import conexao.Conexao;
+
+public class Contador {
+    // Consulta o estoque atual da matéria-prima pelo nome do campo e compara com a quantidade solicitada
+    public static boolean verificarEstoque(String nomeCampo, int quantidadeSolicitada) {
+        int estoqueAtual = 0;
+        String sql = "SELECT " + nomeCampo + " FROM estoquemateriaprima LIMIT 1";
+
+        try (PreparedStatement ps = Conexao.getConexao().prepareStatement(sql)) {
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                estoqueAtual = rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        if (estoqueAtual < quantidadeSolicitada) {
+            System.out.println("Estoque insuficiente de " + nomeCampo + ". É necessário comprar mais!");
+            return false;
+        } else {
+            System.out.println("Estoque suficiente de " + nomeCampo + ".");
+            return true;
+        }
+    }
+
+
+    public static void atualizarEstoque(String nomeCampo, int quantidadeUsada) {
+        String sql = "UPDATE estoquemateriaprima SET " + nomeCampo + " = " + nomeCampo + " - ? LIMIT 1";
+        try (PreparedStatement ps = Conexao.getConexao().prepareStatement(sql)) {
+            ps.setInt(1, quantidadeUsada);
+            ps.executeUpdate();
+            System.out.println("Estoque de " + nomeCampo + " atualizado!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erro ao atualizar o estoque de " + nomeCampo);
+        }
+    }
+}
